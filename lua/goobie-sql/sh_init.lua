4e95fd61963f31eb63eebd256a0d2eb9b502da8e
Srlion <srlion32@gmail.com>
2024-10-10 21:58:51 +0300

    Add CROSS_INT_TYPE

--- lua/goobie-sql/sh_init.lua
+++ lua/goobie-sql/sh_init.lua
@@ -46,6 +46,7 @@ local CROSS_SYNTAXES = {
         CROSS_COLLATE_BINARY = "COLLATE BINARY",
         CROSS_CURRENT_DATE = "DATE('now')",
         CROSS_OS_TIME_TYPE = "INT UNSIGNED NOT NULL DEFAULT (CAST(strftime('%s', 'now') AS INTEGER))",
+        CROSS_INT_TYPE = "INTEGER",
     },
     mysql = {
         CROSS_NOW = "(UNIX_TIMESTAMP())",
@@ -53,6 +54,7 @@ local CROSS_SYNTAXES = {
         CROSS_COLLATE_BINARY = "BINARY",
         CROSS_CURRENT_DATE = "CURDATE()",
         CROSS_OS_TIME_TYPE = "INT UNSIGNED NOT NULL DEFAULT (UNIX_TIMESTAMP())",
+        CROSS_INT_TYPE = "BIGINT",
     },
 }
 
